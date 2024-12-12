336cb64364587f599f6fb334eb16e7b4651902f7
Ramelstag <114973757+Ramelstag@users.noreply.github.com>
2024-12-12 23:34:25 +0000

    initial commit

---
+++ mkdesktop.py
@@ -0,0 +1,45 @@
+import sys
+import os
+import shutil
+
+#bullshit handling
+if len(sys.argv) > 2:
+    sys.exit("Too many arguments. please enter only a single file's name.")
+
+if len(sys.argv) < 2:
+    sys.exit("File not specified")
+
+if not os.path.exists(sys.argv[1]):
+    sys.exit("Error: file " + sys.argv[1] + " does not exit")
+
+if os.path.isdir(sys.argv[1]):
+    sys.exit("Error: " + sys.argv[1] + " is a directory")
+
+
+file = open(sys.argv[1] + ".desktop", "w")
+#header and name
+file.write("[Desktop Entry]\n" + "Name=" + str.capitalize(sys.argv[1]) + "\nType=Application\n")
+#exec
+file.write("Exec=" + os.getcwd() + "/" + sys.argv[1] + "\n")
+#path
+file.write("Path=" + os.getcwd() + "\n")
+#less important stuff (needlessly concatenated yes i know shut up)
+file.write("Icon=\n" + "Comment=\n" + "Category=\n")
+file.close
+
+os.system("chmod +x " + sys.argv[1]) #making sure the target is executable
+
+if os.path.exists("/home/" + os.environ["USER"] + "/.local/share/applications"): #sanity check
+
+    option = input("Would you like to move the output .desktop to ~/.local/share/applications? [y/N]")
+    
+    if option.lower() in ["yes", "y"]:
+        print("Moving output to local applications folder...")
+        shutil.move(sys.argv[1] + ".desktop", "/home/" + os.environ["USER"] + "/.local/share/applications/" + sys.argv[1] + ".desktop")
+        print("Done. You may want to change the Name line, add a comment and an icon.")
+    
+    
+    elif option.lower() in ["no", "n"]:
+        print("Done.")
+    else:
+        print("Defaulted to no. Done.")
